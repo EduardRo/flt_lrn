a3820df4c31b5efaf34628b55a80f47a317a2438
EduardRo <eduardstancu@yahoo.com>
2022-10-03 01:47:29 +0300

    appbottombar main01

---
+++ lib/appbar_bottom_v01/main01.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'this is my title',
+      home: MyStatefulWidget(),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({super.key});
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('AppBar Title')),
+      body: const Center(
+        child: Text(
+          'this is center',
+        ),
+      ),
+    );
+  }
+}
