d3100e57e7b4fc43e9e662869d5c23388d99082b
EduardRo <eduardstancu@yahoo.com>
2022-10-02 14:57:54 +0300

    appbar bottom v03

--- lib/appbar_bottom_v01/main.dart
+++ lib/appbar_bottom_v01/main.dart
@@ -27,7 +27,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   int _selectedIndex = 0;
   static const TextStyle optionStyle =
       TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
+  static const List<Widget> _widgetOptions = [
     Text(
       'Index 0: Home',
       style: optionStyle,
@@ -42,6 +42,10 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
     ),
     Text(
       'Index 3: Settings',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 4: Settings',
       style: optionStyle,
     ),
   ];
@@ -63,6 +67,11 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'Scoala',
+            backgroundColor: Colors.black,
+          ),
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
             label: 'Home',
